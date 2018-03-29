dacf7162fe9e0241cdc9fe12aa343a3e69192977
Chris Vetrano <cvetrano@grubhub.com>
2018-03-29 18:27:58 -0400

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (PlayerController))]
+public class Player : MonoBehaviour {
+
+    public float moveSpeed = 5;
+
+    //reference to camera
+    Camera viewCamera;
+    PlayerController controller;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<PlayerController> ();
+        viewCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //GetAxisRaw instead of GetAxis
+        // avoids smoothing.  Was moving the character after input was let go
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+
+        // moveVelocity exists in PlayerController;
+        controller.Move(moveVelocity);
+        // creates a ray that goes from the camera through
+        // the screen position and on to infinity
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+        // now need to find ground Plane
+        // create a plane progrmatically
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+        // Planes have a Raycast method
+        if (groundPlane.Raycast(ray, out rayDistance)) {
+            Vector3 point = ray.GetPoint(rayDistance);
+            // draw line with debug
+            //Debug.DrawLine(ray.origin, point, Color.red);
+            controller.LookAt(point);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+    //public variables
+    Vector3 velocity;
+    Rigidbody myRigidBody;
+
+	// Use this for initialization
+	void Start () {
+        myRigidBody = GetComponent<Rigidbody>();
+	}
+	
+    public void Move(Vector3 _velocity) {
+        velocity = _velocity;
+    }
+
+    public void LookAt(Vector3 lookPoint) {
+        Vector3 heightCorrectedPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+        // rotate object to look at a point
+        transform.LookAt(heightCorrectedPoint);
+    }
+
+    // needs to be executed at small regular steps
+    // so it doesn't go through any objects
+    // slow frame rates could cause this issue
+    public void FixedUpdate() {
+        // add current position and velcoity.  multiplied by
+        // the time inbetween the calls to fixed update
+        myRigidBody.MovePosition(myRigidBody.position + velocity * Time.fixedDeltaTime);
+    }
+}
